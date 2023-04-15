bb7454ccafbc4836bd70ca11a5fa1ecd8a4468dc
Gal Hikri <galhi@mta.ac.il>
2023-04-15 11:02:37 +0300

    deleting old files

--- src/components/About.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "../css/About.css";
-
-export default class About extends Component {
-  render() {
-    return <div className="about">Hello, this is our about page</div>;
-  }
-}
